ec8e30ec2fc8751e2a7f6a294a474bf344314f48
NotThatJonas <mr.joshuajonas@gmail.com>
2019-09-04 20:46:32 -0700

    created more functions for cleaner code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,13 @@
 var wins = 0;
 var losses = 0;
 var randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-var randomStoneNumber = Math.floor(Math.random() * (12 - 1) + 1);
 var totalScore = 0;
 var stone1 = Math.floor(Math.random() * (12 - 1) + 1);
 var stone2 = Math.floor(Math.random() * (12 - 1) + 1);
 var stone3 = Math.floor(Math.random() * (12 - 1) + 1);
 var stone4 = Math.floor(Math.random() * (12 - 1) + 1);
+
+
 
 console.log('random', randomNumber);
 console.log('random', randomStoneNumber);
@@ -27,61 +28,32 @@ $('#random-number').text(randomNumber);
 $('#stone1').on('click', function () {
     totalScore += stone1; 
     $('#total-score').text(totalScore); 
-    if (totalScore === randomNumber) {
-        wins++;
-        $('#game-wins').text(wins);  
-        restart(); 
-    }; 
-    if (totalScore > randomNumber) {
-        losses++;
-        $('#game-losses').text(losses);
+        victoryCheck();
         restart();
-    };
 });
 
 $('#stone2').on('click', function () {
     totalScore += stone2;
     $('#total-score').text(totalScore); 
-    if (totalScore === randomNumber) {
-        wins++;
-        $('#game-wins').text(wins);   
+        victoryCheck();
         restart();
-    }; 
-    if (totalScore > randomNumber) {
-        losses++;
-        $('#game-losses').text(losses);
-        restart();
-    };     
+   
 });
 
 $('#stone3').on('click', function () {
     totalScore += stone3;
     $('#total-score').text(totalScore);  
-    if (totalScore === randomNumber) {
-        wins++;
-        $('#game-wins').text(wins);
-        restart();   
-    }; 
-    if (totalScore > randomNumber) {
-        losses++;
-        $('#game-losses').text(losses);
+        victoryCheck();
         restart();
-    };
+
 });
 
 $('#stone4').on('click', function () {
     totalScore += stone4;  
     $('#total-score').text(totalScore); 
-    if (totalScore === randomNumber) {
-        wins++;
-        $('#game-wins').text(wins); 
-        restart();  
-    }; 
-    if (totalScore > randomNumber) {
-        losses++;
-        $('#game-losses').text(losses);
+        victoryCheck();
         restart();
-    }; 
+
 });
 
 
@@ -91,23 +63,41 @@ function restart () {
         $('#total-score').text(0);
         randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
         $('#random-number').text(randomNumber);
-        stone1 = Math.floor(Math.random() * (12 - 1) + 1);
-        stone2 = Math.floor(Math.random() * (12 - 1) + 1);
-        stone3 = Math.floor(Math.random() * (12 - 1) + 1);
-        stone4 = Math.floor(Math.random() * (12 - 1) + 1);
+        stone1 = randomStoneNumber();
+        stone2 = randomStoneNumber();
+        stone3 = randomStoneNumber();
+        stone4 = randomStoneNumber();
         totalScore = 0;
     };
 
     if (totalScore === randomNumber) {
         randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
         $('#random-number').text(randomNumber);
-        stone1 = Math.floor(Math.random() * (12 - 1) + 1);
-        stone2 = Math.floor(Math.random() * (12 - 1) + 1);
-        stone3 = Math.floor(Math.random() * (12 - 1) + 1);
-        stone4 = Math.floor(Math.random() * (12 - 1) + 1);
+        stone1 = randomStoneNumber();
+        stone2 = randomStoneNumber();
+        stone3 = randomStoneNumber();
+        stone4 = randomStoneNumber();
         totalScore = 0;
         $('#total-score').text(0); 
     };
 
 
 };
+
+function victoryCheck () {
+    if (totalScore === randomNumber) {
+        wins++
+        $('#game-wins').text(wins)
+    }
+    if (totalScore > randomNumber) {
+        losses++
+        $('#game-losses').text(losses)
+    }
+};
+
+function randomStoneNumber () {
+    var randomNumb = Math.floor(Math.random() * (12 - 1) + 1)
+    return randomNumb;
+}
+
+
